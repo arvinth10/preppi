c4158b6f5f9d87b357d187b50b83768685d37c09
Arvinth Vijayanathan <arvinth3@hotmail.com>
2018-01-20 11:55:07 -0500

    changed functions to different file

--- preppi-action/functions/index.js
+++ preppi-action/functions/index.js
@@ -10,9 +10,6 @@ process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 const fb_database = require('./database.js');
-const prettydiff = require("prettydiff");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
 // a. the action name from the make_name Dialogflow intent
   // const NAME_ACTION = 'make_name';
@@ -65,57 +62,6 @@ exports.preppi = functions.https.onRequest((request, response) => {
   }
 
 
-  //Returns an array of missed words
-function getDifferenceMissedArray (correctText, speechText){
-
-  var args = {
-    source : correctText,
-    diff : speechText,
-    lang : "text"
-
-  };
-
-  var output = prettydiff.api(args); 
-
-  const htmlOut = new JSDOM(output[0]);
-  var missedWordsElem =  htmlOut.window.document.getElementsByClassName('diff-left')[0].querySelectorAll("em"); // .textContent
-
-  var missedWords  = [];
-  for (var num in missedWordsElem){
-    if (missedWordsElem[num].textContent != '' && missedWordsElem[num].textContent != null){ 
-      missedWords.push(missedWordsElem[num].textContent );
-    }
-
-  }
- 
-return missedWords;
-
-}
-
-//Returns an array of added words
-function getDifferenceAddedArray (correctText, speechText){
-  var args = {
-    source : correctText,
-    diff : speechText,
-    lang : "text"
-  };
-
-  var output = prettydiff.api(args); 
-
-  const htmlOut = new JSDOM(output[0]);
-  var addedWordElem =  htmlOut.window.document.getElementsByClassName('diff-right')[0].querySelectorAll("em"); // "Hello world"
-
-
-  var addedWords = [];
-  for (var num in addedWordElem){
-    if(addedWordElem[num].textContent != '' && addedWordElem[num].textContent != null){
-      addedWords.push(addedWordElem[num].textContent );
-    }
-  }
-  
-return addedWords; 
-
-}
 
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
