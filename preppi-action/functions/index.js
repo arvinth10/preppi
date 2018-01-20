8be079fe87101d42ea6c2ac2599605503dff7673
Mathusan Selvarajah <mathusans52@gmail.com>
2018-01-20 01:47:35 -0500

    Sample index.js for simple firebase function

--- preppi-action/functions/index.js
+++ preppi-action/functions/index.js
@@ -1,8 +1,42 @@
-const functions = require('firebase-functions');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').DialogflowApp;
+const functions = require('firebase-functions');
+
+
+// a. the action name from the make_name Dialogflow intent
+  // const NAME_ACTION = 'make_name';
+const LOAD_ACTION = 'load_document';
+// b. the parameters that are parsed from the make_name intent
+  // const COLOR_ARGUMENT = 'color';
+  // const NUMBER_ARGUMENT = 'number';
+const DOC_NAME_ARGUMENT = 'given-name';
+
+exports.preppi = functions.https.onRequest((request, response) => {
+  const app = new App({request, response});
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
+
+
+// c. The function that generates the silly name
+  function loadDocument (app) {
+    let doc_name = app.getArgument(DOC_NAME_ARGUMENT);
+    //let color = app.getArgument(COLOR_ARGUMENT);
+    app.tell('Alright, your document name is ' +
+      doc_name + ' ' +
+      '! See you next time.');
+  }
+  // d. build an action map, which maps intent names to functions
+  let actionMap = new Map();
+  actionMap.set(LOAD_ACTION, loadDocument);
+
+
+app.handleRequest(actionMap);
+});
